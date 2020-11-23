6e47b2f3715301da35385fc7bf56950de6016fba
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-11-23 18:39:12 -0500

    Create empty method to sendDocument

--- src/views/DocumentacionAlumno.vue
+++ src/views/DocumentacionAlumno.vue
@@ -1,33 +1,31 @@
 <template>
     <div>
-            <div class="d-flex">
-        <div id="sidebar-container" class="bg-primary">
-            <div class="logo">
-                <img src="https://i.imgur.com/Stl4qpM.png" alt="Logotipo de Senati" class="img-fluid mx-auto d-block" width="200px">
-            </div>
-            <div class="user m-3 bg-info rounded-pill">
-                <li>
-                    <a href="#" class=" text-light">
-                        <img style="padding: 10px 5px;" src=" img/user.png " alt="Foto de Perfil" class="img-fluid avatar mr-2"> 001214638
-                    </a>
-                </li>
-            </div>
+        <div class="d-flex">
+            <div id="sidebar-container" class="bg-primary">
+                <div class="logo">
+                    <img src="https://i.imgur.com/Stl4qpM.png" alt="Logotipo de Senati" class="img-fluid mx-auto d-block" width="200px">
+                </div>
+                <div class="user m-3 bg-info rounded-pill">
+                    <li>
+                        <a href="#" class=" text-light">
+                            <img style="padding: 10px 5px;" src=" img/user.png " alt="Foto de Perfil" class="img-fluid avatar mr-2"> 001214638
+                        </a>
+                    </li>
+                </div>
 
-            <div class="menu">
-                <a href="inicio" class="d-block text-light p-3"><i class="fas fa-home mr-2"></i>Inicio</a>
-                <a href="perfil" class="d-block text-light p-3"><i class="fas fa-user mr-2"></i>Perfil</a>
-                <a href="documentacionalumno" class="selected d-block text-light p-3"><i class="far fa-file mr-2"></i>Documentación</a>
-                <a href="#" class="d-block text-light p-3"><i class="far fa-calendar mr-2"></i>Calendario</a>
+                <div class="menu">
+                    <a href="inicio" class="d-block text-light p-3"><i class="fas fa-home mr-2"></i>Inicio</a>
+                    <a href="perfil" class="d-block text-light p-3"><i class="fas fa-user mr-2"></i>Perfil</a>
+                    <a href="documentacionalumno" class="selected d-block text-light p-3"><i class="far fa-file mr-2"></i>Documentación</a>
+                    <a href="#" class="d-block text-light p-3"><i class="far fa-calendar mr-2"></i>Calendario</a>
 
-                <a href="cerrarsesion" class="d-block text-light p-3"><i class="fas fa-sign-out-alt mr-2"></i>Cerrar sesión</a>
+                    <a href="cerrarsesion" class="d-block text-light p-3"><i class="fas fa-sign-out-alt mr-2"></i>Cerrar sesión</a>
+                </div>
             </div>
-        </div>
 
         <div class="w-100">
-
             <div id="content">
                 <section>
-
                     <div class="container py-3">
                         <div>
                             <div class="col-lg-12">
@@ -139,18 +137,12 @@
                             </div>
                         </div>
                     </div>
-
                 </section>
-
-
-
                 <footer class="bg-dark p-3">
                     <p class="text-light text-center">Copyright 2020 SENATI</p>
                 </footer>
             </div>
         </div>
-
-
     </div>
     </div>
 </template>
@@ -204,6 +196,9 @@ export default {
             document.querySelector('#file_3_container').classList.add('border');
             document.querySelector('#file_3_container').classList.add('border-success');
         },
+        sendDocument() {
+            
+        }
     }
 }
 </script>
