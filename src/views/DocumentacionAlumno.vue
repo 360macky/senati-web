04ac6603b5a5f64aa8e7e69f26a8ea43c0396014
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-11-23 13:40:17 -0500

    Create border/alert design system when send documents

--- src/views/DocumentacionAlumno.vue
+++ src/views/DocumentacionAlumno.vue
@@ -34,9 +34,8 @@
                                 <h1>Documentación</h1>
                                 <p class="text-muted">Agregue los documentos correspondientes</p>
                             </div>
-                            <div style=" color: #721c24;
-                            background-color: #f8d7da;
-                            border-color: #f5c6cb; position: relative;padding: 0.75rem 1.25rem; margin-bottom: 1rem;    border: 1px solid transparent;border-radius: 0.25rem; flex-wrap: wrap;-ms-flex-wrap: wrap;">
+                            <div class="alert alert-danger" style="
+                             border-color: #f5c6cb; position: relative;padding: 0.75rem 1.25rem; margin-bottom: 1rem;    border: 1px solid transparent;border-radius: 0.25rem; flex-wrap: wrap;-ms-flex-wrap: wrap;" id="alert">
 
                                 Plazo máximo para subir documentos al sistema: 6 de enero del 2021
                             </div>
@@ -46,7 +45,7 @@
                             <div class="col-lg-12">
                                 <form>
 
-                                    <div class="card mt-3">
+                                    <div class="card mt-3" id="file_1_container">
                                         <div class="card-header">
                                             <h4>DNI</h4>
                                         </div>
@@ -55,7 +54,7 @@
                                                 <div class="row">
                                                     <div class="col">
                                                         <label for="exampleFormControlFile1">Agregue su DNI</label>
-                                                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                                                        <input type="file" class="form-control-file" id="file_1" @change="updateBorderColor_DNI()">
 
                                                         <ul class="list-group mt-3">
                                                             <li class="list-group-item">Foto clara y en alta resolución</li>
@@ -63,7 +62,7 @@
                                                             <li class="list-group-item">Morbi leo risus</li>
                                                         </ul>
 
-                                                        <button type="submit" class="btn btn-info btn-lg mt-3">Enviar</button>
+                                                        <button type="submit" class="btn btn-info btn-lg mt-3" @click.prevent="send()">Enviar</button>
                                                     </div>
 
                                                     <div class="col-lg-4">
@@ -76,7 +75,7 @@
                                         </div>
                                     </div>
 
-                                    <div class="card mt-3">
+                                    <div class="card mt-3" id="file_2_container">
                                         <div class="card-header">
                                             <h4>Certificado de estudios</h4>
                                         </div>
@@ -85,7 +84,7 @@
                                                 <div class="row">
                                                     <div class="col">
                                                         <label for="exampleFormControlFile1">Agregue su certificado</label>
-                                                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                                                        <input type="file" class="form-control-file" id="file_2" @change="updateBorderColor_Estudios()">
 
                                                         <ul class="list-group mt-3">
                                                             <li class="list-group-item">Cras justo odio</li>
@@ -93,7 +92,7 @@
                                                             <li class="list-group-item">Morbi leo risus</li>
                                                         </ul>
 
-                                                        <button type="submit" class="btn btn-info btn-lg mt-3">Enviar</button>
+                                                        <button type="submit" class="btn btn-info btn-lg mt-3" @click.prevent="send()">Enviar</button>
                                                     </div>
 
                                                     <div class="col-lg-4">
@@ -106,7 +105,7 @@
                                         </div>
                                     </div>
 
-                                    <div class="card mt-3">
+                                    <div class="card mt-3" id="file_3_container">
                                         <div class="card-header">
                                             <h4>Proyecto</h4>
                                         </div>
@@ -115,7 +114,7 @@
                                                 <div class="row">
                                                     <div class="col">
                                                         <label for="exampleFormControlFile1">Agregue su proyecto</label>
-                                                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                                                        <input type="file" class="form-control-file" id="file_3" @change="updateBorderColor_Proyecto()">
 
                                                         <ul class="list-group mt-3">
                                                             <li class="list-group-item">Cras justo odio</li>
@@ -123,7 +122,7 @@
                                                             <li class="list-group-item">Morbi leo risus</li>
                                                         </ul>
 
-                                                        <button type="submit" class="btn btn-info btn-lg mt-3">Enviar</button>
+                                                        <button type="submit" class="btn btn-info btn-lg mt-3" @click.prevent="send()">Enviar</button>
                                                     </div>
 
                                                     <div class="col-lg-4">
@@ -158,7 +157,53 @@
 
 <script>
 export default {
-    name: 'DocumentacionAlumno'
+    name: 'DocumentacionAlumno',
+    created() {
+        if (this.getCookie('username') != '122365' || this.getCookie('password') != '123456') {
+            window.location.href = '/';
+        }
+    },
+    methods: {
+        setCookie(cname, cvalue, exdays) {
+            let d = new Date();
+            d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+            let expires = 'expires=' + d.toUTCString();
+            document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+        },
+        getCookie(cname) {
+            let name = cname + '=';
+            let decodedCookie = decodeURIComponent(document.cookie);
+            let ca = decodedCookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return '';
+        },
+        send() {
+            document.querySelector('#alert').classList.remove('alert-danger');
+            document.querySelector('#alert').classList.add('alert');
+            document.querySelector('#alert').classList.add('alert-success');
+            document.querySelector('#alert').innerHTML = "Documento enviado"
+        },
+        updateBorderColor_DNI() {
+            document.querySelector('#file_1_container').classList.add('border');
+            document.querySelector('#file_1_container').classList.add('border-success');
+        },
+        updateBorderColor_Estudios() {
+            document.querySelector('#file_2_container').classList.add('border');
+            document.querySelector('#file_2_container').classList.add('border-success');
+        },
+        updateBorderColor_Proyecto() {
+            document.querySelector('#file_3_container').classList.add('border');
+            document.querySelector('#file_3_container').classList.add('border-success');
+        },
+    }
 }
 </script>
 
