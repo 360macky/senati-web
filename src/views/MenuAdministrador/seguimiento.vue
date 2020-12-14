c16c87bd8a6d1926f3bc37e325e28a6e94c419da
Luap11 <69992375+Luap11@users.noreply.github.com>
2020-12-14 15:24:06 -0500

    correcion seguimiento

--- src/views/MenuAdministrador/seguimiento.vue
+++ src/views/MenuAdministrador/seguimiento.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Container -->
-  <div class="container">
+ <div class="container">
     <div class="row">
       <div class="col-lg-12">
         <h1 class="mt-4">Seguimiento</h1>
@@ -32,21 +32,24 @@
             </div>
 
             <div class="row">
-              <div class="col-lg-4">
+              <div class="col-lg-3">
                 <button class="btn btn-info btn-block">Generar reporte</button>
               </div>
-              <div class="col-lg-4">
+              <div class="col-lg-3">
                 <button class="btn btn-success btn-block">Descargar Excel</button>
               </div>
-              <div class="col-lg-4">
+              <div class="col-lg-3">
                 <button class="btn btn-warning btn-block">Backup</button>
+              </div>
+              <div class="col-lg-3">
+                <button class="btn btn-primary btn-block">Cargar alumnos</button>
               </div>
             </div>
 
             <hr class="my-4">
 
             <!-- Alumnos -->
-            <h4 class="my-3 text-primary">Estado de alumnos</h4>
+            <h4 class="my-3 text-success">Estado de alumnos:</h4>
             <div class="row">
               <div class="col-lg-12">
                 <div class="card-deck">
@@ -55,10 +58,13 @@
                     <div class="card-body">
                       <div class="row">
                         <div class="col-lg-4 d-flex align-items-lg-center justify-content-lg-center">
-                          <h1 class="text-primary">00%</h1>
+                          <div class="text-center">
+                            <h1 class="text-primary">160</h1>
+                            <h5 class="text-primary">alumnos</h5>
+                          </div>
                         </div>
                         <div class="col-lg-8">
-                          <h4>Alumnos activos (<span class="text-danger">A</span>)</h4>
+                          <h4 class="font-weight-bold">Activos (<span class="text-danger">A</span>)</h4>
                           <p>Alumnos con proceso de titulación vigente</p>
                         </div>
                       </div>
@@ -69,13 +75,19 @@
                     <div class="card-body">
                       <div class="row">
                         <div class="col-lg-4 d-flex align-items-lg-center justify-content-lg-center">
-                          <h1 class="text-primary">00%</h1>
+                          <div class="text-center">
+                            <h1 class="text-primary">40</h1>
+                            <h5 class="text-primary">alumnos</h5>
+                          </div>
                         </div>
                         <div class="col-lg-8">
-                          <h4>Alumnos inactivos (<span class="text-danger">F</span>)</h4>
+                          <h4 class="font-weight-bold">Inactivos (<span class="text-danger">F</span>)</h4>
                           <p>Alumnos ya titulados o que nunca se titularon</p>
                         </div>
                       </div>
+                    </div>
+                    <div class="card-footer">
+                      <button class="btn btn-info btn-sm btn-block">Generar reporte</button>
                     </div>
                   </div>
 
@@ -84,7 +96,7 @@
             </div>
 
             <!-- Titulación -->
-            <h4 class="my-3 text-primary">Estado de titulación</h4>
+            <h4 class="my-3 text-success">Estado de titulación:</h4>
             <div class="row">
               <div class="col-lg-12">
                 <div class="card-deck">
@@ -92,42 +104,51 @@
                   <div class="card">
                     <div class="card-body">
                       <div class="row">
-                        <div class="col-lg-12 d-flex align-items-lg-center justify-content-lg-center">
-                          <h1 class="text-primary">00%</h1>
+                        <div class="col-lg-12 d-flex align-items-lg-center justify-content-lg-center mb-3">
+                          <h1 class="text-primary">20%</h1>
                         </div>
                         <div class="col-lg-12">
-                          <h4>Faltante (<span class="text-danger">N</span>)</h4>
-                          <p>ALumno que aún no envía sus los documentos correspondientes para la titulación</p>
+                          <h4 class="font-weight-bold">Faltante (<span class="text-danger">N</span>)</h4>
+                          <p>Alumno que aún no envía sus los documentos correspondientes para la titulación</p>
                         </div>
                       </div>
+                    </div>
+                    <div class="card-footer">
+                      <button class="btn btn-info btn-sm btn-block">Generar reporte</button>
                     </div>
                   </div>
 
                   <div class="card">
                     <div class="card-body">
                       <div class="row">
-                        <div class="col-lg-12 d-flex align-items-lg-center justify-content-lg-center">
-                          <h1 class="text-primary">00%</h1>
+                        <div class="col-lg-12 d-flex align-items-lg-center justify-content-lg-center mb-3">
+                          <h1 class="text-primary">50%</h1>
                         </div>
                         <div class="col-lg-12">
-                          <h4>Pendiente (<span class="text-danger">P</span>)</h4>
-                          <p>ALumno que envío sus documentos correspondientes, pero que aún no han sido revisados.</p>
+                          <h4 class="font-weight-bold">Pendiente (<span class="text-danger">P</span>)</h4>
+                          <p>Alumno que envió sus documentos correspondientes, pero que aún no han sido revisados.</p>
                         </div>
                       </div>
+                    </div>
+                    <div class="card-footer">
+                      <button class="btn btn-info btn-sm btn-block">Generar reporte</button>
                     </div>
                   </div>
 
                   <div class="card">
                     <div class="card-body">
                       <div class="row">
-                        <div class="col-lg-12 d-flex align-items-lg-center justify-content-lg-center">
-                          <h1 class="text-primary">00%</h1>
+                        <div class="col-lg-12 d-flex align-items-lg-center justify-content-lg-center mb-3">
+                          <h1 class="text-primary">30%</h1>
                         </div>
                         <div class="col-lg-12">
-                          <h4>Aprobado (<span class="text-danger">A</span>)</h4>
+                          <h4 class="font-weight-bold">Aprobado (<span class="text-danger">A</span>)</h4>
                           <p>Alumno que cumple con los requisitos para lograr la titulación.</p>
                         </div>
                       </div>
+                    </div>
+                    <div class="card-footer">
+                      <button class="btn btn-info btn-sm btn-block">Generar reporte</button>
                     </div>
                   </div>
 
